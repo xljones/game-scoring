8bdf43d387a1ff959f1cd33405d8a0955286a948
Xander Jones <xander@posthog.com>
2026-04-18 10:44:56 +0100

    fix: check for nulls and empty strings

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "/out";
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "/out";
 
 const nextConfig: NextConfig = {
   output: "export",
